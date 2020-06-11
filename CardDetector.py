cf9f29cc19f3bc16c5233d09e03eb1d531b127a6
Nobert1 <gustav.nobert1998@gmail.com>
2020-06-11 17:40:47 +0200

    kortet bliver nu projekteret rigtigt

--- CardDetector.py
+++ CardDetector.py
@@ -54,6 +54,7 @@ input_from_user = input("If you want to use computer webcam press 1, "
                         "for IP Cam Server press ENTER ")
 if input_from_user == '1':
     cap = cv2.VideoCapture(1)
+    time.sleep(1)
 else:
     pasted_URL = input("Paste the IP Camera Server URL ")
     cap = cv2.VideoCapture(
@@ -73,15 +74,62 @@ while cam_quit == 0:
     t1 = cv2.getTickCount()
     # Pre-process camera image (gray, blur, and threshold it)
     pre_proc = Cards.preprocces_image(frame)
-
     # Find and sort the contours of all cards in the image (query cards)
     cnts_sort, cnt_is_card, crns = Cards.find_cards(pre_proc)
+
     if crns is not None:
-        Cards.preprocess_card(frame, crns)
+        corner1, corner2, w, h, top1, top2, bot1, bot2 = Cards.CalculateCardPosition(crns)
+        cv2.imshow('projektering', frame)
+        cv2.circle(frame, (int(top1[0]), int(top1[1])), 6, (0, 255, 255), -1)
+        cv2.circle(frame, (int(top2[0]), int(top2[1])), 6, (0, 255, 255), -1)
+        cv2.circle(frame, (int(bot1[0]), int(bot1[1])), 6, (0, 0, 255), -1)
+        cv2.circle(frame, (int(bot2[0]), int(bot2[1])), 6, (0, 0, 255), -1)
+        card = Cards.preprocess_card(frame, crns, w, h)
+
+        card.best_rank_match, card.best_suit_match, card.rank_diff, \
+        card.suit_diff = Cards.match_card(card, train_ranks, train_suits)
+
+        # Draw center point and match result on the image.
+        frame = Cards.draw_results(frame, card)
+
+    # Draw framerate in the corner of the image. Framerate is calculated at the end of the main loop,
+    # so the first time this runs, framerate will be shown as 0.
+    cv2.putText(frame, "FPS: " + str(int(frame_rate_calc)), (10, 26), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "KORTBUNKE ", (10, 50), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "GRUNDBUNKER ", (720, 26), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, "BYGGESTABLER ", (10, 500), font, 0.7, (255, 0, 255), 2, cv2.LINE_AA)
+
+    # Draw the lines into the frame for splitting the card piles. This may make it easier to identify cards.
+    cv2.line(frame, (0, 450), (frame.size, 450), BLUE_COLOR, 5)
+    cv2.line(frame, (700, 0), (700, 450), RED_COLOR, 5)
+
+    # Resize the frame.
+    scale_percent = 100  # percent of original size
+    width = int(frame.shape[1] * scale_percent / 100)
+    height = int(frame.shape[0] * scale_percent / 100)
+    dim = (width, height)
+    frame = cv2.resize(frame, dim, interpolation=cv2.INTER_AREA)
+
+    # Finally, display the image with the identified cards!
+    cv2.imshow("Card Detector", frame)
+
+    # cv2.imshow("Preprossed image", Cards.preprocces_image(image))
+
+    # Calculate framerate
+    t2 = cv2.getTickCount()
+    time1 = (t2 - t1) / freq
+    frame_rate_calc = 1 / time1
+
+    # Poll the keyboard. If 'q' is pressed, exit the main loop.
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+        cam_quit = 1
 
 
-    # If there are no contours, do nothing
-    if len(cnts_sort) != 0:
+
+
+    """" if len(cnts_sort) != 0:
 
         # Initialize a new "cards" list to assign the card objects.
         # k indexes the newly made array of cards.
@@ -144,9 +192,9 @@ while cam_quit == 0:
     frame_rate_calc = 1 / time1
 
     # Poll the keyboard. If 'q' is pressed, exit the main loop.
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord("q"):
-        cam_quit = 1
+    key = cv2.waitKey(1) & 0xFF"""
+"""if key == ord("q"):
+        cam_quit = 1"""
 
 # Close all windows and close the IP Camera video stream.
 cap.release()
--- Cards.py
+++ Cards.py
@@ -208,7 +208,7 @@ def find_cards(thresh_image):
     return cnts_sort, cnt_is_card, crns
 
 
-def preprocess_card(image, pts):
+def preprocess_card(image, pts, w, h):
     """Uses contour to find information about the query card. Isolates rank
     and suit images from the card."""
 
@@ -224,7 +224,7 @@ def preprocess_card(image, pts):
     qCard.corner_pts = pts
 
     # Find width and height of card's bounding rectangle
-    x, y, w, h = cv2.boundingRect(pts)
+    # x, y, w, h = cv2.boundingRect(pts)
     qCard.width, qCard.height = w, h
 
     # Find center point of card by taking x and y average of the four corners.
@@ -234,9 +234,9 @@ def preprocess_card(image, pts):
     qCard.center = [cent_x, cent_y]
 
     # Warp card into 200x300 flattened image using perspective transform
-    qCard.warp = flattener(image, pts, w, h)
+    qCard.warp = flattener(image, pts)
 
-    # cv2.imshow("200x300 card", qCard.warp)
+    cv2.imshow("200x300 card", qCard.warp)
 
     # Grab corner of warped card image and do a 4x zoom
     Qcorner = qCard.warp[0:CORNER_HEIGHT, 0:CORNER_WIDTH]
@@ -245,10 +245,6 @@ def preprocess_card(image, pts):
     # Sample known white pixel intensity to determine good threshold level
     white_level = Qcorner_zoom[15, int((CORNER_WIDTH * 4) / 2)]
     thresh_level = white_level - CARD_THRESH
-
-    # if (thresh_level <= 0): # Det her er den originale linje men den giver en fejl.
-    if (thresh_level.any):
-        thresh_level = 1
 
     cv2.imshow('Qcorner', Qcorner_zoom)
 
@@ -347,6 +343,7 @@ def match_card(qCard, train_ranks, train_suits):
     if (best_suit_match_diff < SUIT_DIFF_MAX):
         best_suit_match_name = best_suit_name
 
+
     # Return the identiy of the card and the quality of the suit and rank match
     return best_rank_match_name, best_suit_match_name, best_rank_match_diff, best_suit_match_diff
 
@@ -378,7 +375,7 @@ def draw_results(image, qCard):
     return image
 
 
-def flattener(image, pts, w, h):
+def flattener(image, pts):
     """Flattens an image of a card into a top-down 200x300 perspective.
     Returns the flattened, re-sized, grayed image.
     See www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example/"""
@@ -397,42 +394,62 @@ def flattener(image, pts, w, h):
     # [top left, top right, bottom right, bottom left]
     # before doing the perspective transform
 
-    if w <= 0.8 * h:  # If card is vertically oriented
-        temp_rect[0] = tl
-        temp_rect[1] = tr
-        temp_rect[2] = br
-        temp_rect[3] = bl
+    # h er altid lig med h = 1.5w
 
-    if w >= 1.2 * h:  # If card is horizontally oriented
-        temp_rect[0] = bl
-        temp_rect[1] = tl
-        temp_rect[2] = tr
-        temp_rect[3] = br
+    # if w <= 0.8 * h:  # If card is vertically oriented
 
-    # If the card is 'diamond' oriented, a different algorithm
-    # has to be used to identify which point is top left, top right
-    # bottom left, and bottom right.
+    topcornerlist = np.array([[1000000, 10000000], [100000000, 100000000]])
 
-    if w > 0.8 * h and w < 1.2 * h:  # If card is diamond oriented
-        # If furthest left point is higher than furthest right point,
-        # card is tilted to the left.
-        if pts[1][0][1] <= pts[3][0][1]:
-            # If card is titled to the left, approxPolyDP returns points
-            # in this order: top right, top left, bottom left, bottom right
-            temp_rect[0] = pts[1][0]  # Top left
-            temp_rect[1] = pts[0][0]  # Top right
-            temp_rect[2] = pts[3][0]  # Bottom right
-            temp_rect[3] = pts[2][0]  # Bottom left
+    for corn in pts:
+        if (corn[0][1] < topcornerlist[0][1]):
+            topcornerlist[1] = topcornerlist[0]
+            topcornerlist[0] = corn[0]
 
-        # If furthest left point is lower than furthest right point,
-        # card is tilted to the right
-        if pts[1][0][1] > pts[3][0][1]:
-            # If card is titled to the right, approxPolyDP returns points
-            # in this order: top left, bottom left, bottom right, top right
-            temp_rect[0] = pts[0][0]  # Top left
-            temp_rect[1] = pts[3][0]  # Top right
-            temp_rect[2] = pts[2][0]  # Bottom right
-            temp_rect[3] = pts[1][0]  # Bottom left
+        elif (corn[0][1] < topcornerlist[1][1]):
+            topcornerlist[1] = corn
+
+    bottomcornerlist = np.array([[0, 0], [0, 0]])
+    for corn in pts:
+        if (corn[0][1] > bottomcornerlist[0][1]):
+            bottomcornerlist[1] = bottomcornerlist[0]
+            bottomcornerlist[0] = corn[0]
+
+        elif (corn[0][1] > bottomcornerlist[1][1]):
+            bottomcornerlist[1] = corn
+
+    print('top1')
+    print(topcornerlist[0])
+
+    print('top2')
+    print(topcornerlist[1])
+
+    print('bot1')
+    print(bottomcornerlist[0])
+
+    print('bot2')
+    print(bottomcornerlist[1])
+    # Please være søde ikke at rette i det her - Gustav
+
+    # Hvis kortet hælder mod højre
+    # top1 = venstre hjørne, top2 = højre
+    # bot1 = højre hjørne bot2 = venstre hjørne
+    if (topcornerlist[0][0] < topcornerlist[1][0]):
+        temp_rect[0] = topcornerlist[0]
+        temp_rect[1] = topcornerlist[1]
+        temp_rect[2] = bottomcornerlist[0]
+        temp_rect[3] = bottomcornerlist[1]
+    # Nu skifter vi. Den der har den laveste y værdi er nu modsatte hjørne.
+    else:
+        temp_rect[0] = topcornerlist[1]
+        temp_rect[1] = topcornerlist[0]
+        temp_rect[2] = bottomcornerlist[1]
+        temp_rect[3] = bottomcornerlist[0]
+        # if (bottomcornerlist[0][1] < bottomcornerlist[1][1]):
+        #    temp_rect[0] = br
+       #     temp_rect[3] = bl
+       # else:
+        #    temp_rect[0] = br
+        #    temp_rect[3] = bl
 
     maxWidth = 200
     maxHeight = 300
@@ -448,16 +465,14 @@ def flattener(image, pts, w, h):
 
 def CalculateCardPosition(crns):
     cornerlist = np.array([[0, 0], [0, 0]])
+
     for corn in crns:
         if (corn[0][1] > cornerlist[0][1]):
-            cornerlist[1][0] = cornerlist[0][0]
-            cornerlist[1][1] = cornerlist[0][1]
+            cornerlist[1] = cornerlist[0]
+            cornerlist[0] = corn[0]
 
-            cornerlist[0][0] = corn[0][0]
-            cornerlist[0][1] = corn[0][1]
         elif (corn[0][1] > cornerlist[1][1]):
-            cornerlist[1][0] = corn[0][0]
-            cornerlist[1][1] = corn[0][1]
+            cornerlist[1] = corn
 
 
     vector = None
@@ -468,14 +483,17 @@ def CalculateCardPosition(crns):
         vector = cornerlist[0] - cornerlist[1]
 
     orthogonal_vector = [-1.5*vector[1], 1.5*vector[0]]
+    # width
+    w = math.sqrt(math.pow(vector[0],2) + math.pow(vector[1],2))
+    # height
+    h = w*1.5
 
     topcorner1 = cornerlist[0] + orthogonal_vector
     topcorner2 = cornerlist[1] + orthogonal_vector
 
-    print('corner1')
-    print(topcorner1)
-    print('corner2')
-    print(topcorner2)
-
-
-    return topcorner1, topcorner2, cornerlist[0], cornerlist[1]
+    if (cornerlist[0][0] > cornerlist[0][1] ):
+        mincorner = min(topcorner1[0], topcorner2[0])
+        return cornerlist[0], mincorner, w, h, topcorner1, topcorner2, cornerlist[0], cornerlist[1]
+    else:
+        maxcorner = max(topcorner1[1], topcorner2[1])
+        return cornerlist[1], maxcorner, w, h, topcorner1, topcorner2, cornerlist[0], cornerlist[1]
